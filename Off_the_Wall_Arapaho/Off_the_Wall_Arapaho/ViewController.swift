0cde212c6fe97a903c187460954cf89cda03c6e8
Yadira Valadez <yadivaladez@gmail.com>
2017-01-20 20:26:19 -0700

    Updating launchscreen

---
+++ Off_the_Wall_Arapaho/Off_the_Wall_Arapaho/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Off_the_Wall_Arapaho
+//
+//  Created by Yadira Valadez on 1/18/17.
+//  Copyright © 2017 Yadira Valadez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
